caa210753dda0ef4884f73a1ff8b6c932e25244c
inamkkkk <inamk9025@gmail.com>
2026-01-03 19:45:25 +0500

    added things here we have

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,15 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('../../gas.json');
-if (serviceAccount.private_key) {
-  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
-}
+// if (serviceAccount.private_key) {
+//   serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+// }
 
 try {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     storageBucket: 'gaschahiye-c6f00.firebasestorage.app'
   });
-  
+
   console.log('Firebase Admin initialized successfully');
 } catch (error) {
   console.error('Firebase initialization error:', error);
