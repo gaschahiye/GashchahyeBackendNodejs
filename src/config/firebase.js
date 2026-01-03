d1fc0dffe09787971faadd354d5ece4bf6cf658a
inamkkkk <inamk9025@gmail.com>
2026-01-03 19:25:38 +0500

    added things here too

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,8 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('../../gas.json');
-
+if (serviceAccount.private_key) {
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+}
 
 try {
   admin.initializeApp({
